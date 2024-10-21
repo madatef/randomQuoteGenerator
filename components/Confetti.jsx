a432d2ec366308f41be34b8170a3cf385b66c409
madatef <madatef1@outlook.com>
2024-10-22 01:19:33 +0300

    trivials

--- components/Confetti.jsx
+++ components/Confetti.jsx
@@ -1,15 +1,20 @@
 'use-client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Confetti from 'react-confetti'
 import { useWindowSize } from 'react-use';
 
 
 const MyConfetti =  () => {
+    let width, height;
+    useEffect(() => {
+        width = window.innerWidth;
+        height = window.innerHeight;
+    })
 
   return (
     <Confetti
-      width={window.innerWidth}
-      height={window.innerHeight}
+      width={width}
+      height={height}
     />
   )
 }
